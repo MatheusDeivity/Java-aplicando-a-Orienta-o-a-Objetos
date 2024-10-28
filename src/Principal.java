1f5a24899275fa183ec69c54f5750b624dbcfb6b
matheus.gomes <matheus.silva8481@gmail.com>
2024-10-28 13:25:28 -0300

    Aula 7 alura

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,5 @@
+import br.com.alura.screematch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -9,8 +11,10 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: "+ meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+        //meuFilme.somaDasAvaliacoes = 10;
+        //meuFilme.totalDeAvaliacoes = 1;
         System.out.println(meuFilme.pegaMedia());
     }
 }
